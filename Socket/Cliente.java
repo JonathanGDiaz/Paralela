063229fb4444df689d607ff3c12a0408361d50ff
JonathanGDiaz <morokeidovah@gmail.com>
2022-11-16 00:20:18 -0600

    Adicion del Socket lineal

---
+++ Socket/Cliente.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * Cliente
+ */
+public class Cliente {
+    public static final int PUERTO = 1111;
+    public static final String HOST = "localhost";
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Host " + HOST);
+        Socket socket = new Socket(HOST, PUERTO);
+        Scanner in = new Scanner(System.in);
+        try {
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter salida = new PrintWriter(
+                    new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+            while (true) {
+                String str = in.nextLine();
+                salida.println("Usuario: soy tu cliente " + str);
+                System.out.println("Mensaje enviado al server");
+                if (str.equals("FIN")) {
+                    salida.println(str);
+                    break;
+                }
+            }
+        } finally {
+            in.close();
+            System.out.println("Cerrando el changarro");
+            socket.close();
+        }
+    }
+
+}
---
+++ Socket/Servidor.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Servidor
+ */
+public class Servidor {
+    public static final int PUERTO = 1111;
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket S = new ServerSocket(PUERTO);
+        System.out.println("Empezando " + S);
+        try {
+            Socket socket = S.accept();
+            try {
+                System.out.println("Conexion aceptada " + socket);
+                BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter salida = new PrintWriter(
+                        new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+                while (true) {
+                    String str = entrada.readLine();
+                    if (str.equals("FIN"))
+                        break;
+                    System.out.println("Reproduciendo " + str);
+                    salida.println(str);
+                }
+            } finally {
+                System.out.println("Cerrando");
+                socket.close();
+            }
+        } finally {
+            S.close();
+        }
+    }
+}
